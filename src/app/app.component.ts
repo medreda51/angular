50b92b2abf4616a7d2a96e88ba5b17cf83ec91e6
Mohammed Reda <medreda51@gmail.com>
2023-03-17 20:24:38 +0100

    first commit day 20230317

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'clientes-app';
+  title = 'Bienvenido a Angular';
+  curso: string = 'Curso Spring con Angular 9';
+  profesor: string = 'Mohammed Reda';
 }
---
+++ src/app/clientes/clientes.json.ts
@@ -0,0 +1,14 @@
+import { Cliente } from './cliente';
+export const CLIENTES: Cliente[] =[
+    {id:1, nombre:'MOHAMMED1 REDA', apellido:'HAMDAN1 ', email: 'test1@gmail.com', createAt: '2023-03-15'},
+    {id:2, nombre:'MOHAMMED2 REDA', apellido:'HAMDAN2 ', email: 'test2@gmail.com', createAt: '2023-03-14'},
+    {id:3, nombre:'MOHAMMED3 REDA', apellido:'HAMDAN3 ', email: 'test3@gmail.com', createAt: '2023-03-13'},
+    {id:4, nombre:'MOHAMMED4 REDA', apellido:'HAMDAN4 ', email: 'test4@gmail.com', createAt: '2023-03-12'},
+    {id:5, nombre:'MOHAMMED5 REDA', apellido:'HAMDAN5 ', email: 'test5@gmail.com', createAt: '2023-03-10'},
+    {id:6, nombre:'MOHAMMED6 REDA', apellido:'HAMDAN6 ', email: 'test6@gmail.com', createAt: '2023-03-09'},
+    {id:7, nombre:'MOHAMMED7 REDA', apellido:'HAMDAN7 ', email: 'test7@gmail.com', createAt: '2023-03-08'},
+    {id:8, nombre:'MOHAMMED8 REDA', apellido:'HAMDAN8 ', email: 'test8@gmail.com', createAt: '2023-03-07'},
+    {id:9, nombre:'MOHAMMED9 REDA', apellido:'HAMDAN9 ', email: 'test9@gmail.com', createAt: '2023-03-06'},
+    {id:10, nombre:'MOHAMMED10 REDA', apellido:'HAMDAN10 ', email: 'test10@gmail.com', createAt: '2023-03-05'},
+    {id:11, nombre:'MOHAMMED11 REDA', apellido:'HAMDAN11 ', email: 'test11@gmail.com', createAt: '2023-03-04'},
+  ]
